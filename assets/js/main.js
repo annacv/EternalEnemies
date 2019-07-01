31e4270ed45b819dac5d2ccd14c9333c6b2ec44a
annacv <21217131+annacv@users.noreply.github.com>
2019-07-01 18:47:11 +0200

    Main js file to load game screens & keyboard events

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,63 @@
 'use strict'
 
-// SCREENS: 
+function main() {
+  var mainElement = document.querySelector('#site-main');
 
-// 1 splash screen
-// 2 play - game
-// 3 game over
+  function buildDom(html) {
+    mainElement.innerHTML = html;
+    return mainElement;
+  };
 
-// * build Dom();
+  function createSplashScreen() {
+    var splashScreen = buildDom(`
+      <section>
+        <h1>Eternal Enemies</h1>
+        <button>Start</button>
+      </section>
+    `);
+
+    var startButton = splashScreen.querySelector('button');
+    startButton.addEventListener('click', createGameScreen);
+  };
+
+  function createGameScreen() {
+    var gameScreen = buildDom(`
+      <section>
+        <canvas width="400px" height="400"></canvas>
+      </section>
+    `);
+
+    var canvas = document.querySelector('canvas');
+    var game = new Game(canvas);
+
+    game.gameOverCallback(createGameOverScreen);
+    
+    game.startGame();
+
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'ArrowDown') {
+        game.player.setDirection(1);
+      } else if (event.key === 'ArrowUp') {
+        game.player.setDirection(-1);
+      }
+
+    });
+    //setTimeout(createGameOverScreen, 3000);
+  };
+
+  function createGameOverScreen() {
+    var gameOverScreen = buildDom(`
+      <section>
+        <h1>Game Over</h1>
+        <button>Restart</button>
+      </section>
+    `);
+
+    var restartButton = gameOverScreen.querySelector('button');
+    restartButton.addEventListener('click', createGameScreen);
+  };
+
+  createSplashScreen();
+}
+
+window.addEventListener('load', main);
